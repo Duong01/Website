da667fa84704480337cc0f0f85724145c804c833
Duong01 <86581230+Duong01@users.noreply.github.com>
2022-11-26 23:16:56 +0700

    Delete Tym directory

--- Tym/script.js
+++
@@ -1,7 +0,0 @@
-for(let i=0;i<1;i++){
-    let span=document.createElement('span');
-    span.style.left=Math.random() *innerWidth 
-    + 'px';
-    span.style.animationDelay=Math.random() *5+'s';
-    document.body.appendChild(span);
-}
